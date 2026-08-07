c6795483f7b36eeb36e9463f0a0bc17e11c4c4b5
Yagyesh Agarwal <yagyesh127@gmail.com>
2026-08-07 14:05:09 +0530

    minor code changes  for cap changes

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bisleri.vasai',
   appName: 'Bisleri Vasai Store',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+  url: 'https://e-commerce-ap-5jq4.vercel.app',
+  cleartext: false
+}
 };
 
 export default config;
